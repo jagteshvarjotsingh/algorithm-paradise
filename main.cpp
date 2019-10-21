ef66bbe00f292e17ccf27bb414291f493036d9b4
jagteshvarjotsingh <55827320+jagteshvarjotsingh@users.noreply.github.com>
2019-10-21 15:53:50 +0530

    jot swap entry

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  swap.c
+//
+//  Created by Jagteshvar Jot Singh on 21/10/19.
+//  Copyright © 2019 Jagteshvar Jot Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
